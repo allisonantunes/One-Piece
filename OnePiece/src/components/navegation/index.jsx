c4a75b2008927ec077b227eecc004f3528c136d1
allison <allisonabc@hotmail.com>
2023-09-29 09:05:52 -0300

    part2

---
+++ OnePiece/src/components/navegation/index.jsx
@@ -0,0 +1,11 @@
+export function Nav() {
+    return (
+        <ul>
+            <li><button class="botao"><img src="./src/imagens/tripulacao-chopper.png" alt="tripulacao chopper" /></button></li>
+            <li><button class="botao"><img src="./src/imagens/tripulacao-zoro.png" alt="tripulacao zoro" /></button></li>
+            <li><button class="botao selecionado"><img src="./src/imagens/tripulacao-luffy.png" alt="tripulacao luffy" /></button></li>
+            <li><button class="botao"><img src="./src/imagens/tripulacao-nami.png" alt="tripulacao nami" /></button></li>
+            <li><button class="botao"><img src="./src/imagens/tripulacao-sanji.png" alt="tripulacao sanji" /></button></li>
+        </ul>
+    )
+}
